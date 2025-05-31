e026b9acb1e40ade2d18720ff6b585d70052f17a
sindi791 <ncendekiar@gmail.com>
2025-06-01 00:21:57 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,15 +1,17 @@
 
-import re, string
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
+import re
+import string
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 
+# Hardcoded stopwords (tanpa NLTK)
 stop_words = set([
     'yang', 'dan', 'di', 'ke', 'dari', 'ini', 'untuk', 'dengan', 'pada', 'karena',
     'juga', 'akan', 'itu', 'sudah', 'saja', 'lagi', 'kalau', 'bisa', 'kami', 'kita',
     'mereka', 'ada', 'tidak', 'ya', 'kok', 'gak', 'ga', 'jadi', 'terus', 'dapat', 'lah',
     'apa', 'kenapa', 'nah', 'oh', 'iya', 'terima', 'kasih', 'thanks', 'thank'
 ])
+
+# Stemmer
 stemmer = StemmerFactory().create_stemmer()
 
 def preprocess_text(text):
@@ -17,6 +19,6 @@ def preprocess_text(text):
     text = re.sub(r"http\S+|@\w+|#\w+|\d+", "", text)
     text = text.translate(str.maketrans("", "", string.punctuation))
     text = re.sub(r"\s+", " ", text).strip()
-    tokens = word_tokenize(text)
+    tokens = text.split()  # Gantikan word_tokenize
     filtered = [stemmer.stem(word) for word in tokens if word not in stop_words]
     return " ".join(filtered)
